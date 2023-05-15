9668dbb645799ed2aa51ce4f9c30ef1a9a3cba64
Aaisha Chaudhary <aaisha7.ac@gmail.com>
2023-05-15 17:17:22 +0530

    first commit

---
+++ 2nd_Semester/String/string_is_palindrome_or_not.c
@@ -0,0 +1,51 @@
+// Given a string, you need to test if it is palindrome or not. 
+// If the string is palindrome print “YES”, otherwise print “NO”.
+
+
+// TC : O(len) len = length of string
+// SC : O(1)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Return 1 if the string is palindrome, else return 0
+int isPalindrome(char *str) {
+  // Write your code here
+  int len = strlen(str) - 1;
+  int i = 0;
+  while(i <= len) {
+    if(str[i] != str[len]) {
+      return 0;
+    }
+    i++;
+    len--;
+  }
+  return 1;
+}
+
+int main() 
+{
+  int t,i,j=0;
+  char a[50], b[50];
+  scanf("%d",&t);
+  while(t--)
+  {
+    scanf("%s", a);
+    if(isPalindrome(a)){
+      printf("YES");
+    }else{
+      printf("NO");
+    }
+    printf("\n");
+  }
+}
+
+// Sample Input
+// 2
+// Coding
+// cooc
+
+// Sample Output
+// NO
+// YES
+
